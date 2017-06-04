53ca8e908a37fe87b7b500cc179fa5f0553f7f49
Ferdinand Bermudez <code@ferdinandbermudez.com>
2017-06-05 00:24:16 +0200

    chore: use semantic versioning

--- src/main/java/com/ferdinandbermudez/dragoneggrecipe/DragonEggRecipe.java
+++ src/main/java/com/ferdinandbermudez/dragoneggrecipe/DragonEggRecipe.java
@@ -11,7 +11,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 @Mod(modid = DragonEggRecipe.MODID, version = DragonEggRecipe.VERSION)
 public class DragonEggRecipe {
     public static final String MODID = "DragonEggRecipe";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.0";
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
