83140627f05e662604a69b0ac180d750185e1a4c
Ferdinand Bermudez <code@ferdinandbermudez.com>
2017-06-09 23:04:22 +0200

    chore: upgrade to 1.11.2

--- src/main/java/com/ferdinandbermudez/dragoneggrecipe/DragonEggRecipe.java
+++ src/main/java/com/ferdinandbermudez/dragoneggrecipe/DragonEggRecipe.java
@@ -3,21 +3,21 @@ package com.ferdinandbermudez.dragoneggrecipe;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(modid = DragonEggRecipe.MODID, version = DragonEggRecipe.VERSION)
 public class DragonEggRecipe {
-    public static final String MODID = "DragonEggRecipe";
-    public static final String VERSION = "1.0.0";
+    public static final String MODID = "dragoneggrecipe";
+    public static final String VERSION = "1.1.0";
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        ItemStack dragonEgg = new ItemStack(Blocks.dragon_egg);
-        ItemStack dragonEggCore = new ItemStack(Items.nether_star);
-        ItemStack dragonEggShell = new ItemStack(Blocks.obsidian);
+        ItemStack dragonEgg = new ItemStack(Blocks.DRAGON_EGG);
+        ItemStack dragonEggCore = new ItemStack(Items.NETHER_STAR);
+        ItemStack dragonEggShell = new ItemStack(Blocks.OBSIDIAN);
         GameRegistry.addRecipe(dragonEgg, "sss", "scs", "sss", 'c', dragonEggCore, 's', dragonEggShell);
     }
 }
