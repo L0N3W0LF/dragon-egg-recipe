e1c8fe4d3f66f3e7dadc633f2925c96ae2cb59af
Ferdinand Bermudez <code@ferdinandbermudez.com>
2019-06-07 22:13:38 +0200

    chore: bump mod version to 1.2.0

--- src/main/java/com/ferdinandbermudez/dragoneggrecipe/DragonEggRecipe.java
+++ src/main/java/com/ferdinandbermudez/dragoneggrecipe/DragonEggRecipe.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 @Mod(modid = DragonEggRecipe.MODID, version = DragonEggRecipe.VERSION)
 public class DragonEggRecipe {
     public static final String MODID = "dragoneggrecipe";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.2.0";
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
